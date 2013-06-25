bfa353c55a862ec9a81a6de93494fca715dfeee7
Hidenori Ogawa <hide.seaweed@gmail.com>
2013-06-25 23:11:28 +0900

    Sign up user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+  def current_user
+  	@current_user ||= User.find(session[:current_user_id]) if session[:current_user_id]
+  	@current_user
+  end
+  helper_method :current_user
+
+  def current_user=(user)
+  	session[:current_user_id] = user.id
+  end
+  helper_method :current_user=
 end
