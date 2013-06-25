f882ada386578e21ea89278f555f77750d8ef63d
Hidenori Ogawa <h_ogawa@h228.csg.ci.i.u-tokyo.ac.jp>
2013-06-25 21:26:55 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Syukeikun::Application.config.secret_token = 'bd4cdea08507dc8174a97b3a246f35fd06e68e24eb614d2e4bfdb3327686d3fd4cb5eb7b0112993090d612c916cfb8ac68eaabb9e4bd1cabbf41d230cc480a90'
